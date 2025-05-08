64b0c73679baa73f4c216914914d70b5021e36d3
Wesley <83589196+Wesley534@users.noreply.github.com>
2025-05-08 17:41:09 +0300

    Delete backend/h.py

--- backend/h.py
+++
@@ -1,5 +0,0 @@
-from passlib.context import CryptContext
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-password = "Qwerty!234"  # Replace with the desired password
-hashed_password = pwd_context.hash(password)
-print(hashed_password)
